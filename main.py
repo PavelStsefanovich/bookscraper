0211ef3d62800e3875b44ae595ebb5ea6902fa71
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2020-12-04 23:16:47 -0800

    Commented out command line argument that is not implemented

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from subprocess import Popen
 ######## USER INPUT #######################################
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-u','--url', dest='url', help='Target book URL')
-parser.add_argument('-t','--title', dest='title', help='Book title (will also be optimized and used as output file name)')
+#TODO implement title override from cmdargs
+#parser.add_argument('-t','--title', dest='title', help='Book title (will also be optimized and used as output file name)')
 parser.add_argument('-s','--start_page', dest='start_page', default='1', help='First page to be included into scraping')
 parser.add_argument('-e','--end_page', dest='end_page', help='Last page to be included into scraping')
 parser.add_argument('-p','--params_file', dest='params_file', default='params.yml', help='Path to YAML parameters file (to be used instead of command-line arguments; if both parameters file and cmd arguments specified, cmd arguments take priority)')
